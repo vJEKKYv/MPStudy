de137a6aa391104540fb79f3deb70ad41f6135a5
JEKKY <wpdnjs20@gmail.com>
2024-05-03 14:05:09 +0900

    11-4 목록 화면 구성 완성

--- ch11-4/src/main/java/com/example/ch11_4/MainActivity.kt
+++ ch11-4/src/main/java/com/example/ch11_4/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ch11_4
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ch11_4.databinding.ActivityMainBinding
@@ -17,20 +19,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val datas = mutableListOf<String>()
-        for (i in 1..10){
+        for (i in 1..5){
             datas.add("Item $i")
         }
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = MyAdapter(datas)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this,LinearLayoutManager.VERTICAL))
+        binding.addBtn.setOnClickListener{
+            datas.add("new data")
+            adapter.notifyDataSetChanged()
+        }
     }
 }
 class MyViewHolder(val binding: ItemMainBinding):RecyclerView.ViewHolder(binding.root)
 
-class MyAdapter(val datas:MutableList<String>):RecyclerView.Adapter<RecyclerView.ViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent,false))
-    }
+class MyAdapter(val datas:MutableList<String>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
     override fun getItemCount(): Int {
         return datas.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+        binding.itemData.text = datas[position]
+        binding.itemData.setOnClickListener{
+            Log.d("kkang", "Item Click : $position")
+        }
     }
 }
